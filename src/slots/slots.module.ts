9b839682eaa259ad4f5de135bf52708c798dea70
ralf303 <nikitasuhanov@gmail.com>
2025-04-06 17:46:01 +0300

    add userService

--- src/slots/slots.module.ts
+++ src/slots/slots.module.ts
@@ -4,10 +4,16 @@ import { SlotsController } from './slots.controller';
 import { SlotegratorService } from './services/slotegrator.service';
 import { SignatureService } from './services/signature.service';
 import { CallbackService } from './services/callback.service';
+import { UserService } from 'src/user/user.service';
 
 @Module({
   imports: [HttpModule],
   controllers: [SlotsController],
-  providers: [SlotegratorService, SignatureService, CallbackService],
+  providers: [
+    SlotegratorService,
+    SignatureService,
+    CallbackService,
+    UserService,
+  ],
 })
 export class SlotsModule {}
