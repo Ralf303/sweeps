a12c2ba5e7f4b56dca08b602ba8c76d9ebef109d
ralf303 <nikitasuhanov@gmail.com>
2025-04-24 21:39:12 +0300

    update url

--- src/slots/services/slotegrator.service.ts
+++ src/slots/services/slotegrator.service.ts
@@ -81,7 +81,7 @@ export class SlotegratorService {
   async getValidate(): Promise<any> {
     const { headers } = this.signatureService.generateHeaders();
     const response = await firstValueFrom(
-      this.httpService.post(`${this.apiUrl}/validate`, {
+      this.httpService.post(`${this.apiUrl}/self-validate`, {
         headers,
       }),
     );
