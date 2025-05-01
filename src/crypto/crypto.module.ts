2e874990384a30a24afe27453dcaea6127a4a15e
ralf303 <nikitasuhanov@gmail.com>
2025-05-01 23:17:54 +0300

    fix redis

--- src/crypto/crypto.module.ts
+++ src/crypto/crypto.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CryptoService } from './crypto.service';
 import { CryptoController } from './crypto.controller';
+import { RedisService } from 'src/redis/redis.service';
 
 @Module({
   controllers: [CryptoController],
-  providers: [CryptoService],
+  providers: [CryptoService, RedisService],
 })
 export class CryptoModule {}
