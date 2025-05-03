a13bd9aed0890ef506bb4048e0e0dd271e600aea
ralf303 <nikitasuhanov@gmail.com>
2025-05-03 23:20:43 +0300

    remove type 2x

--- src/socials/socials.service.ts
+++ src/socials/socials.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { SocialLink } from '@prisma/client';
 
 @Injectable()
 export class SocialsService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async getAll(): Promise<SocialLink[]> {
+  async getAll(): Promise<any> {
     return this.prisma.socialLink.findMany();
   }
 
-  async updateLink(id: number, url: string): Promise<SocialLink> {
+  async updateLink(id: number, url: string): Promise<any> {
     return this.prisma.socialLink.update({
       where: { id },
       data: { url },
