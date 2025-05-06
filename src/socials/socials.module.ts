86e6b5d6bcbb9911c42f7d3c1175a231703fdb46
ralf303 <nikitasuhanov@gmail.com>
2025-05-06 20:12:24 +0300

    add jwt

--- src/socials/socials.module.ts
+++ src/socials/socials.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SocialsService } from './socials.service';
 import { SocialsController } from './socials.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [SocialsController],
-  providers: [SocialsService, PrismaService],
+  providers: [SocialsService, PrismaService, JwtService],
 })
 export class SocialsModule {}
