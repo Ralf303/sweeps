945b57545a92d7e31252e134aa9d01ae706cc0ae
Ralf303 <nikitasuhanov34@gmail.com>
2025-05-17 16:18:54 +0300

    jwtService add

--- src/crypto/crypto.module.ts
+++ src/crypto/crypto.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CryptoService } from './crypto.service';
 import { CryptoController } from './crypto.controller';
 import { RedisService } from 'src/redis/redis.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [CryptoController],
-  providers: [CryptoService, RedisService],
+  providers: [CryptoService, RedisService, JwtService],
 })
 export class CryptoModule {}
